33935d3441fa3e3927e06789bcde6168ef56baca
GavinJZhu <gzhu100@icloud.com>
2022-04-08 11:48:48 -0700

    4/4/2022 Made a square, a colored in circle, and a colored in nonagon! Also made the nonagon seperated from the circle and square. 4/5/2022 made turtle spiral, octagon, and rick astley have a moustache, lol. 4/6/2022 made a rainbow crazy pentagon, scripted my longest for loop, made a rainbow ninja star, and made a huge turtle spin and change colors, huge progress ig. 4/7/2022 pop ups 4/8/2022 made a quiz game, and a uh, fortune teller?

--- _03_print_and_popups/_c_world_domination.py
+++ _03_print_and_popups/_c_world_domination.py
@@ -1,15 +1,19 @@
 from tkinter import messagebox, simpledialog, Tk
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
+if __name__=='__main__':
     # Make a new window variable, window = Tk()
-    
+    window = Tk()
     # Hide the window using the window's .withdraw() method
-    
+    window.withdraw()
     # 1. Ask the user if they know how to write code.
-    
+    UA = simpledialog.askstring(title='Hmmmmm', prompt= "Know how 2 code?" )
     # 2. If they say "yes", tell them they will rule the world in a message box pop-up.
-
+    if UA == 'yes':
+        messagebox.showinfo(title= 'Uh oh..', message= "Well have fun ruling the world...   ( ⁰д⁰)" )
+    # 2. If they say
     # 3. Otherwise, tell them to sign up for classes at The League in an error box pop-up.
-    
+    else:
+        messagebox.showerror(title= 'What r u doing?', message= "Sign up for The League to learn... now!")
     # Run the window's .mainloop() method
+    window.mainloop()
--- _03_print_and_popups/_d_quiz_game.py
+++ _03_print_and_popups/_d_quiz_game.py
@@ -1,22 +1,35 @@
 from tkinter import messagebox, simpledialog, Tk
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
+if __name__ == '__main__':
     # Make a new window variable, window = Tk()
-    
+    window = Tk()
     # Hide the window using the window's .withdraw() method
-    
+    window.withdraw()
     # 1. Create a variable to hold the user's score. Set it equal to zero. 
-
+    Score = 0
     # ASK A QUESTION AND CHECK THE ANSWER
-
+    math = simpledialog.askinteger(title= 'Hmmm1', prompt= "whats 48.355x36?   (round)")
     #      // 2.  Ask the user a question 
-    
-    #      // 3.  Use an if statement to check if their answer is correct
 
+    #      // 3.  Use an if statement to check if their answer is correct
+    if math==1741:
     #      // 4.  if the user's answer was correct, add one to their score 
- 
-    # MAKE MORE QUESTIONS. Ask more questions by repeating the above 
+        Score = Score+1
+
+
+    build = simpledialog.askstring(title= 'Hmmmm2', prompt= "Whats the tallest building?")
+    if build=="burj khalifa":
+        Score = Score+2
+
+
+    history = simpledialog.askstring(title= 'Hmmmmm3', prompt= "When did christopher Columbus come to the Americas ")
+    if history==1492:
+        Score = Score+3
+
+
+    print(Score)
+    # MAKE MORE QUESTIONS. Ask more questions by repeating the above
     #      // Option: Subtract a point from their score for a wrong answer
  
     # After all the questions have been asked, tell the user their final score
